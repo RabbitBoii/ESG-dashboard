3a11fa7bd21f83eb4cf623aceb1a29c605614469
RabbitBoii <chetanatram11284@gmail.com>
2026-05-28 02:51:24 +0530

    updated requirements.txt + postgres config

--- ESG/core/settings.py
+++ ESG/core/settings.py
@@ -66,9 +66,12 @@ TEMPLATES = [
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        engine='django.db.backends.postgresql',
+    )
 }
-
 
 CORS_ALLOW_ALL_ORIGINS = True  # tighten this in prod
 
